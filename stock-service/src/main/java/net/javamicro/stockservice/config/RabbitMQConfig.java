86440cf230d688b37e27db90ff8b0faa187799ca
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-28 15:24:49 +0200

    Configure rabbitMQ to stock-service then create RabbitMQ Consumer

---
+++ stock-service/src/main/java/net/javamicro/stockservice/config/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package net.javamicro.stockservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    //no need for create Bean queue, exchange and binding from here. because we created these in order-service.
+    // this is a consumer, we need to read json object,so we need 2 methods to convert and read json object here
+    @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    //configure RabbitTemplate
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
